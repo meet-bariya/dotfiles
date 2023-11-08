67a47057d8cd5f2ea195d05c7c8de8fb16d55d90
Meet Bariya <marutimeet@gmail.com>
2023-11-08 17:18:10 +0530

    dwm source repo added

---
+++ .local/src/dwmblocks/config.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+
+/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+  {"",  "volume", 1,  0},
+  {"",  "internet", 5,  4},
+  {"",  "battery", 1, 1},
+  {"",  "clock",  60, 1},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
