439aa08cc713740c2891d82fdf52ea4abe48a597
Meet Bariya <marutimeet@gmail.com>
2023-12-01 21:48:43 +0530

    removed unnecessary directories

--- .local/src/dwm/config.h
+++ .local/src/dwm/config.h
@@ -30,10 +30,10 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class                instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "mpv",                NULL,     NULL,           1 << 3,    1,          0,           0,        -1 },
+	{ "mpv",                NULL,     NULL,           0,	     1,          0,           0,        -1 },
 //	{ "obs",                NULL,     NULL,           0,         1,          0,           0,        -1 },
 	{ "Lutris",             NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "firefox",   		NULL,     NULL,           1 << 2,    0,          0,          -1,        -1 },
+	{ "firefox",   		NULL,     NULL,           0,         0,          0,          -1,        -1 },
 	{ "St",                 NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,                 NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
